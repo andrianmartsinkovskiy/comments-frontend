1411b0bf70b1525cd1d3e2cfba076baaf83416ea
Andrian Martsinkovskiy <andrianmartsinkovskiy@MacBook-Air-Andrian.local>
2023-09-06 19:49:50 +0300

    first commit

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const axiosRequest = axios.create({
-  baseURL: 'http://localhost:8001/'
+  baseURL: 'http://localhost:8001/',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 
 export { axiosRequest }
